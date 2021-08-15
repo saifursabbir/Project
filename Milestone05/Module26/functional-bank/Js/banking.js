c6fe53e6eeea9e5cc35a438f5ece33a503e0a550
saifursabbir <theesabbir@gmail.com0>
2021-08-16 04:59:57 +0600

    added

---
+++ Milestone05/Module26/functional-bank/Js/banking.js
@@ -0,0 +1,72 @@
+
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    // clear input filed
+    inputField.value = '';
+    return amountValue;
+};
+
+function updateTotalField(totalFieldId, amount) {
+    const totalElement = document.getElementById(totalFieldId);
+    const totalText = totalElement.innerText;
+    const previousTotal = parseFloat(totalText);
+
+    totalElement.innerText = previousTotal + amount;
+}
+
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    if (isAdd == true) {
+        balanceTotal.innerText = previousBalanceTotal + amount;
+    }
+    else {
+        balanceTotal.innerText = previousBalanceTotal - amount;
+    }
+};
+
+document.getElementById('diposit-button').addEventListener('click', function () {
+    /* const dipositInput = document.getElementById('diposit-input');
+      const dipositAmountText = dipositInput.value;
+      const dipositAmount = parseFloat(dipositAmountText);*/
+    // get and update diposit total
+    /* const dipositTotal = document.getElementById('diposit-total');
+     const dipositTotalText = dipositTotal.innerText;
+     const previousDipositTotal = parseFloat(dipositTotalText);
+     dipositTotal.innerText = previousDipositTotal + dipositAmount;
+    */
+    // Update Balance
+    /* const balanceTotal = document.getElementById('balance-total');
+     const balanceTotalText = balanceTotal.innerText;
+     const previousBalanceTotal = parseFloat(balanceTotalText);
+     balanceTotal.innerText = previousBalanceTotal + dipositAmount;
+     */
+    const dipositAmount = getInputValue('diposit-input');
+    updateTotalField('diposit-total', dipositAmount);
+    updateBalance(dipositAmount, true);
+});
+
+// handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    /*const withdrawInput = document.getElementById('Withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);*/
+    //get and update withdraw total
+    /*  const withdrawTotal = document.getElementById('withdraw-total');
+      const previousWithdrawTotalText = withdrawTotal.innerText;
+      const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+      withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+  */
+    // update balance after withdraw 
+    /*   const balanceTotal = document.getElementById('balance-total');
+       const balanceTotalText = balanceTotal.innerText;
+       const previousBalanceTotal = parseFloat(balanceTotalText);
+       balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+   */
+    const withdrawAmount = getInputValue("Withdraw-input");
+    updateTotalField('withdraw-total', withdrawAmount);
+    updateBalance(withdrawAmount, false);
+});
